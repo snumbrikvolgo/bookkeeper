6957fb2d0010fb2c4fefb0fd6d467ab9ef1130c9
Elizaveta Zazhigina <snumbrikvolgo@gmail.com>
2023-03-12 01:46:47 +0300

    requirements done for bookkeeper/models

--- bookkeeper/models/budget.py
+++ bookkeeper/models/budget.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 from bookkeeper.repository.abstract_repository import AbstractRepository
 from bookkeeper.models.expense import Expense
 
+
 @dataclass(slots=True)
 class Budget:
     """
@@ -19,11 +20,11 @@ class Budget:
     """
     limitation: int
     period: str
-    spent: str = 0
-    pk: str = 0
+    spent: int = 0
+    pk: int = 0
 
-    def __init__(self, limitation: int, period: str, 
-                       spent: str = 0, pk: str = 0):
+    def __init__(self, limitation: int, period: str,
+                 spent: int = 0, pk: int = 0):
         if period not in ["day", "week", "month"]:
             raise ValueError(f'unknown period "{period}" for budget'
                              + 'should be "day", "week" or "month"')
@@ -32,13 +33,14 @@ class Budget:
         self.spent = spent
         self.pk = pk
 
-
     def update_spent(self, exp_repo: AbstractRepository[Expense]) -> None:
+        """
+        Обновить бюджет, посмотрев на все траты
+        """
         date = datetime.now().isoformat()[:10]
-        print(exp_repo.get_all())
         if self.period.lower() == "day":
             date_mask = f"{date}"
-            period_exps = exp_repo.get_all_like(like={"expense_date":date_mask})
+            period_exps = exp_repo.get_all_like(like={"expense_date": date_mask})
         elif self.period.lower() == "week":
             weekday_now = datetime.now().weekday()
             day_now = datetime.fromisoformat(date)
@@ -47,8 +49,8 @@ class Budget:
             for i in range(7):
                 weekday = first_week_day + timedelta(days=i)
                 date_mask = f"{weekday.isoformat()[:10]}"
-                period_exps += exp_repo.get_all_like(like={"expense_date":date_mask})
+                period_exps += exp_repo.get_all_like(like={"expense_date": date_mask})
         elif self.period.lower() == "month":
             date_mask = f"{date[:7]}-"
-            period_exps = exp_repo.get_all_like(like={"expense_date":date_mask})
-        self.spent = sum([int(exp.amount) for exp in period_exps])
+            period_exps = exp_repo.get_all_like(like={"expense_date": date_mask})
+        self.spent = sum(int(exp.amount) for exp in period_exps)
